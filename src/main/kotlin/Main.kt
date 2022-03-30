0abba41aa84ca90d2c6ff4dab60b6d36fdcbccc2
zacha <20094535@mail.wit.ie>
2022-03-30 01:24:02 +0100

    Raw strings

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,20 +6,21 @@ fun main(args: Array<String>) {
 }
 val scanner = Scanner(System.`in`)
 
+
 fun mainMenu() : Int {
-    println("")
-    println("--------------------")
-    println("NOTE KEEPER APP")
-    println("--------------------")
-    println("NOTE MENU")
-    println("  1) Add a note")
-    println("  2) List all notes")
-    println("  3) Update a note")
-    println("  4) Delete a note")
-    println("--------------------")
-    println("  0) Exit")
-    println("--------------------")
-    print("==>> ")
+    print(""" 
+         > ----------------------------------
+         > |        NOTE KEEPER APP         |
+         > ----------------------------------
+         > | NOTE MENU                      |
+         > |   1) Add a note                |
+         > |   2) List all notes            |
+         > |   3) Update a note             |
+         > |   4) Delete a note             |
+         > ----------------------------------
+         > |   0) Exit                      |
+         > ----------------------------------
+         > ==>> """.trimMargin(">"))
     return scanner.nextInt()
 }
 
