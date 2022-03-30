892639b060cacfa7de08a0f4be8e4af19dc04a33
zacha <20094535@mail.wit.ie>
2022-03-30 01:25:20 +0100

    String Templates

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -33,7 +33,7 @@ fun runMenu() {
             3  -> updateNote()
             4  -> deleteNote()
             0  -> exitApp()
-            else -> System.out.println("Invalid option entered: " + option)
+            else -> System.out.println("Invalid option entered: ${option}")
         }
     } while (true)
 }
