59aadf1186fe919467134efb8155e31e18ba5d7c
zacha <20094535@mail.wit.ie>
2022-03-30 01:09:06 +0100

    Adding menu

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,60 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.lang.System.exit
+import java.util.*
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+    runMenu()
 }
+val scanner = Scanner(System.`in`)
+
+fun mainMenu() : Int {
+    println("")
+    println("--------------------")
+    println("NOTE KEEPER APP")
+    println("--------------------")
+    println("NOTE MENU")
+    println("  1) Add a note")
+    println("  2) List all notes")
+    println("  3) Update a note")
+    println("  4) Delete a note")
+    println("--------------------")
+    println("  0) Exit")
+    println("--------------------")
+    print("==>> ")
+    return scanner.nextInt()
+}
+
+fun runMenu() {
+    do {
+        val option = mainMenu()
+        when (option) {
+            1  -> addNote()
+            2  -> listNotes()
+            3  -> updateNote()
+            4  -> deleteNote()
+            0  -> exitApp()
+            else -> System.out.println("Invalid option entered: " + option)
+        }
+    } while (true)
+}
+
+fun addNote(){
+    println("You chose Add Note")
+}
+
+fun listNotes(){
+    println("You chose List Notes")
+}
+
+fun updateNote(){
+    println("You chose Update Note")
+}
+
+fun deleteNote(){
+    println("You chose Delete Note")
+}
+
+fun exitApp(){
+    println("Exiting...bye")
+    exit(0)
+}
+
